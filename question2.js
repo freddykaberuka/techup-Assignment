279c6e0c22ac73f293c9fddf004e964b66855cd0
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-02 12:47:29 +0200

    update question 2

--- question2.js
+++ question2.js
@@ -1,24 +1,19 @@
-const item = [ {name: 'Bike', price:100}, {name: 'TV', price:200}, {name: 'Album', price:10}, {name: 'Book', price:5}, {name: 'Phone', price:500}, {name: 'Computer', price:1000}, ];
+const items = [ {name: 'Bike', price:100}, {name: 'TV', price:200}, {name: 'Album', price:10}, {name: 'Book', price:5}, {name: 'Phone', price:500}, {name: 'Computer', price:1000}, ];
 
-// 1
-const cheapestItems = item.filter((el) => {
-    return el.price <= 5;
-});
+// 1 Filter and show the product that will be bought when you don't have much money I mean Cheap one
+// i assume that the price of the product is less than 6$.
+const cheapestItems = items.filter((item) => item.price <= 5);
 console.log('Cheap Items: ', cheapestItems);
 
-// 2
-const expensiveItems = item.filter((el) => {
-    return el.price >= 100;
-});
+// 2 Filter and show the product that will be expensive in the array
+// i assume that the price of the product is more than 100$.to become exprensive
+const expensiveItems = items.filter((item) => item.price >= 100);
 console.log('Expensive items: ', expensiveItems);
 
-// 3
-const fullPrice = item.reduce((currentSum, el) => {
-    return el.price + currentSum;
-}, 0);
+// 3 Calculate the full price of all product combined
+const fullPrice = items.reduce((currentSum, item) => item.price + currentSum, 0);
 console.log(`Full price of all products = `, fullPrice);
 
-const itemRemove = item.filter(el => { return el.price > 10 })
-console.log(itemRemove.reduce((currentSum, el) => {
-    return el.price + currentSum;
-}, 0));
+// 4 Calculate the full price of all product combined and remove product that are under the 10 dollar
+const itemRemove = items.filter(item => item.price > 10);
+console.log(`Full price of all products - products under 10$ = `,itemRemove.reduce((currentSum, item) =>  item.price + currentSum, 0));
